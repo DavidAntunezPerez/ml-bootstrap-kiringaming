4616633dfeaf496e9c3588233d9bca4082b97af0
DavidAntunezPerez <antunezdavid2003@gmail.com>
2022-02-09 12:10:17 +0100

    Added Copyright Tooltip in Footer Section

---
+++ javascripts/script.js
@@ -0,0 +1,5 @@
+// BS5 TOOLTIPS
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
